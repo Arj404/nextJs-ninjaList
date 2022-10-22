7b1d95c31d3a91c466a7134369cab3e9a69bc0a1
Arjav Jain <arjavjain598@gmail.com>
2022-10-22 20:20:40 +0530

    basic setup

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='logo'>
+                <h1>Ninja List</h1>
+                <div>
+                    <a>Home</a>
+                    <a>Ninja Listing</a>
+                    <a>About</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <h2>Homepage</h2>
+      <Footer/>
     </div>
   )
 }
