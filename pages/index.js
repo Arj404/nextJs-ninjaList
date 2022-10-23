de63e945227a02ddaef1a5cc8241831a60a969c6
Arjav Jain <31507107+Arj404@users.noreply.github.com>
2022-10-23 23:38:51 +0530

    custom 404 added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h2>Homepage</h2>
+      <h2 className={styles.title}>Homepage</h2>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
       <Link href="ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
