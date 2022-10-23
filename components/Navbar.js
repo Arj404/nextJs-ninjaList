296d2a65c7b434a15c5adc2581f2876fd0ff4359
Arjav Jain <arjavjain598@gmail.com>
2022-10-23 23:17:25 +0530

    layout added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,14 @@
+import Link from 'next/link'
+
 const Navbar = () => {
     return (
         <nav>
             <div className='logo'>
                 <h1>Ninja List</h1>
-                <div>
-                    <a>Home</a>
-                    <a>Ninja Listing</a>
-                    <a>About</a>
                 </div>
-            </div>
+                <Link href="/"><a>Home</a></Link>
+                <Link href="ninjas"><a>Ninja Listing</a></Link>
+                <Link href="about"><a>About</a></Link>
         </nav>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/footer'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h2>Homepage</h2>
-      <Footer/>
+      <Link href="ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
     </div>
-  )
+  );
 }
